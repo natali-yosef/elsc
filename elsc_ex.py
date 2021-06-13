ce1a306bde9caff8a305aa9cf028403efedcb4a2
natali-yosef <59454353+natali-yosef@users.noreply.github.com>
2021-06-13 14:29:44 +0300

    elsc_ex.py
    adding loops and making sure that the same photo doesn't appear twice

--- elsc_ex.py
+++ elsc_ex.py
@@ -1,40 +1,47 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import time
-
 import random
-
 import os
 from psychopy import visual
-
 import ctypes
 
+BLOCKS_CATEGORY = ['faces', 'cars', 'cars_scrambled', 'faces_scrambled']
+FOLDERS_CATEGORY = ['faces', 'cars']
+NUM_BLOCKS = 4
+NUM_TRIALS = 40
+PEEKED_PHOTOS = []
+
+# checking the sizes of the screen
 user32 = ctypes.windll.user32
 Width, Height = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 
-win = visual.Window((Width, Height))
+win = visual.Window((Width, Height))  # creating the window
 
-# text_screen = visual.TextStim(win,"text3")
+text_screen = visual.TextStim(win, "אני")  # creating a text on the screen
 # button = visual.ButtonStim(win,"start",pos=(0,0))
-
-foo = ['faces', 'cars']
-
-random_choice = random.choice(foo)
-path = r"C:\Users\נטלי\PycharmProjects\pythonProject1"+"/"+random_choice
-random_filename = random.choice([x for x in os.listdir(path)
-                                 if os.path.isfile(os.path.join(path, x))])
-
-image = visual.ImageStim(win, random_choice+ "/"+random_filename)
-
-
-# text_screen.draw()
+text_screen.draw()
 win.flip()
-time.sleep(3)
-image.draw()
-win.flip()
-time.sleep(3)
+time.sleep(0.2)
 
+
+for block in range(NUM_BLOCKS):  #
+    block_choice = random.choice(BLOCKS_CATEGORY)  # peeking randomly category for block
+    BLOCKS_CATEGORY.remove(block_choice)  # making sure that we won't peek same category twice
+
+    for trial in range(NUM_TRIALS):
+        random_category = random.choice(FOLDERS_CATEGORY)  # peeking randomly category
+        random_photo = random.choice(
+            [x for x in os.listdir(random_category) if os.path.isfile(os.path.join(random_category, x))])
+        while random_photo in PEEKED_PHOTOS:  # making sure that we won't choose the same photo twice
+            random_photo = random.choice(
+                [x for x in os.listdir(random_category) if os.path.isfile(os.path.join(random_category, x))])
+        PEEKED_PHOTOS.append(random_photo)
+        image = visual.ImageStim(win, random_category + "/" + random_photo)
+        image.draw()
+        win.flip()
+        time.sleep(0.4)
+
+
+win.flip()
+time.sleep(0.4)
 
 
