541a098102fd5e6ea3496b03615efa5d5dfe1c14
natali-yosef <59454353+natali-yosef@users.noreply.github.com>
2021-06-15 21:34:46 +0300

    elsc_ex.py
    Transfer of results to csv file (excel)

--- elsc_ex.py
+++ elsc_ex.py
@@ -11,7 +11,9 @@ import csv
 BLOCKS_CATEGORY = ['faces', 'cars', 'cars_scrambled', 'faces_scrambled']
 FOLDERS_CATEGORY = ['faces', 'cars']
 NUM_BLOCKS = 4
-NUM_TRIALS = 40
+# NUM_TRIALS = 40
+NUM_TRIALS = 2
+STIMULUS_DURATION = 400  # how long was the picture shown on screen in milliseconds
 PEEKED_PHOTOS = []
 
 
@@ -20,35 +22,32 @@ def creating_window():
     user32 = ctypes.windll.user32
     width, height = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
     win = visual.Window((width, height))  # creating the window
-
-    text_screen = visual.TextStim(win, "אני")  # creating a text on the screen
-    # button = visual.ButtonStim(win,"start",pos=(0,0))
-    text_screen.draw()
     win.flip()
     time.sleep(0.2)
     return win
 
 
-def showing_photos():
+def showing_photos(writer):
+    block_number, trial_number = 0, 0
     im = visual.ImageStim(win, "instructions.png", size=[2, 2])  # handling with the instructions
     im.draw()
     win.flip()
     event.waitKeys()  # wait for some button pressing
 
     for block in range(NUM_BLOCKS):
-        block_choice = random.choice(BLOCKS_CATEGORY)  # peeking randomly category for block
-        BLOCKS_CATEGORY.remove(block_choice)  # making sure that we won't peek the same category twice
+        target_category = random.choice(BLOCKS_CATEGORY)  # peeking randomly category for block
+        BLOCKS_CATEGORY.remove(target_category)  # making sure that we won't peek the same category twice
         text_screen1 = visual.TextStim(
-                                    win,
-                                    "the category is:  " + block_choice,
-                                    pos=(0, 0),
-                                    color='white')  # creating a text on the screen
+            win,
+            "the category is:  " + target_category,
+            pos=(0, 0),
+            color='white')  # creating a text on the screen
         button = visual.ButtonStim(
-                                    win,
-                                    "start",
-                                    size=[0.1, 0.1],
-                                    pos=(0, -0.3),
-                                    color='red')
+            win,
+            "start",
+            size=[0.1, 0.1],
+            pos=(0, -0.3),
+            color='red')
         button.draw()
         text_screen1.draw()
         win.flip()
@@ -56,34 +55,58 @@ def showing_photos():
             continue
         time.sleep(0.2)  # giving the user some extra time
 
-        for trial in range(2):
+        for trial in range(NUM_TRIALS):
             random_category = random.choice(FOLDERS_CATEGORY)  # peeking randomly category
-            random_photo = random.choice(
-                                        [x for x in os.listdir(random_category)
-                                        if os.path.isfile(os.path.join(random_category, x))])
-            while random_photo in PEEKED_PHOTOS:  # making sure that we won't choose the same photo twice
-                random_photo = random.choice(
+            image_presented = random.choice(
+                [x for x in os.listdir(random_category)
+                 if os.path.isfile(os.path.join(random_category, x))])
+            while image_presented in PEEKED_PHOTOS:  # making sure that we won't choose the same photo twice
+                image_presented = random.choice(
                     [x for x in os.listdir(random_category) if os.path.isfile(os.path.join(random_category, x))])
-            PEEKED_PHOTOS.append(random_photo)
-            image = visual.ImageStim(win, random_category + "/" + random_photo, size=[0.6, 0.9])
+            PEEKED_PHOTOS.append(image_presented)
+            image = visual.ImageStim(win, random_category + "/" + image_presented, size=[0.6, 0.9])
             image.draw()
             win.flip()
             time.sleep(0.4)
             button = visual.ButtonStim(
-                                        win,
-                                        text='',
-                                        size=[0.05, 0.1],
-                                        pos=(0, 0))
+                win,
+                text='',
+                size=[0.05, 0.1],
+                pos=(0, 0))
             button.draw()
             win.flip()
-            user_result = event.getKeys(keyList=['space'], modifiers=False)
+            key_list = event.getKeys(keyList=['space'], modifiers=False)
+            if 'space' in key_list:  # checking if the user pressed 'space'
+                user_response = True  # True if he pressed
+            else:
+                user_response = False  # False if not
+            if target_category == random_category:  # checking if the photo is from the target category
+                correct_response = True  # True if yes
+            else:
+                correct_response = False  # False if not
             time.sleep(0.6)
-
+            response_time = 0
+            writer.writerow(
+                [block_number, target_category, trial_number, image_presented, random_category, STIMULUS_DURATION,
+                 user_response, correct_response, response_time])
+            # with results_file:
+            #     writer = csv.writer(results_file)
+            #     writer.writerow(
+            #         [block_number, target_category, trial_number, image_presented, random_category, STIMULUS_DURATION,
+            #          user_response, correct_response, response_time])
+            trial_number += 1
+        trial_number = 0
+        block_number += 1
     # win.flip()
     # time.sleep(0.4)
 
 
 if __name__ == '__main__':
     win = creating_window()
-    showing_photos()
+    results_file = open('results.csv', 'w', newline='')
+    writer = csv.writer(results_file)
+    writer.writerow(['block_number', 'target_category', 'trial_number', 'image_presented', 'present_ca_image',
+                     'stimulus_duration', 'user_response', 'correct_response', 'response_time'])
+    showing_photos(writer)
+    results_file.close()
     win.close()
